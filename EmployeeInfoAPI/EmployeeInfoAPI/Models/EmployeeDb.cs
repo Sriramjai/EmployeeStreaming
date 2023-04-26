933bff347e26e38524dcfa8fcf0d4ac758c16cf7
Anjamani <sanjamani@griffithfoods.com>
2023-04-25 20:59:13 -0400

    Adding EmployeeInfoAPI project

---
+++ EmployeeInfoAPI/EmployeeInfoAPI/Models/EmployeeDb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeInfoAPI.Models;
+
+[Keyless]
+[Table("EmployeeDB")]
+public partial class EmployeeDb
+{
+    public int EmployeeNumber { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string EmployeeName { get; set; } = null!;
+
+    [Column(TypeName = "decimal(18, 0)")]
+    public decimal HourlyRate { get; set; }
+
+    [Column(TypeName = "decimal(18, 0)")]
+    public decimal HoursWorked { get; set; }
+
+    [Column(TypeName = "decimal(18, 0)")]
+    public decimal? TotalPay { get; set; }
+}
