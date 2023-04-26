170408b814e4633dd7ad2395bffda0e7de62511b
Sriramjai <53799236+Sriramjai@users.noreply.github.com>
2023-04-25 21:16:23 -0400

    Delete EmployeeInfoAPI directory

--- EmployeeInfoAPI/EmployeeInfoAPI/Data/CursorParams.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EmployeeInfoAPI.Data
-{
-    public class CursorParams
-    {
-        public int Count { get; set; } = 50; // setting the number of records to 50 as default
-        public int Cursor { get; set; } = 0;
-    }
-}
